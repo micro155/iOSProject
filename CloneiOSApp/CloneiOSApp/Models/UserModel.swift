4437d9a537d401206bfa07814e1b41fce3978245
micro155 <qawe2006@naver.com>
2021-08-07 20:45:11 +0900

    Firebase 유저모델 구성

--- CloneiOSApp/CloneiOSApp/Models/UserModel.swift
+++ CloneiOSApp/CloneiOSApp/Models/UserModel.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 
-struct User {
+struct User: Encodable, Decodable {
     var uid: String
     var email: String
     var profileImageUrl: String
     var username: String
-    var searchName: String
+    var searchName: [String]
     var bio: String
 }
